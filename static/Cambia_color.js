e680d01be3b00ca1a618b3fc8d0fcb924ea96e81
IsaacFarah-hub <isaacfarah123@gmail.com>
2025-03-04 12:57:39 -0500

    Update Cambia_color.js

--- static/Cambia_color.js
+++ static/Cambia_color.js
@@ -1,13 +1,13 @@
 
-const numberElement = document.getElementById("sensor");
-const circleElement = document.querySelector(".circulo");
-const uvBar = document.getElementById("rango");
+const numberElement = document.getElementById("sensor"); // Toma el ID "sensor" del html para trabajar con el
+const circleElement = document.querySelector(".circulo");// Toma la clase "circulo" del html para trabajar con el
+const uvBar = document.getElementById("rango"); //Toma el ID "rango" del html para trabajar con el
 
-function updateNumberAndUvBar(newNumber) {
+function updateNumberAndUvBar(newNumber) { //Funcion para el cambio de color segun el indice UV
 
-    if (newNumber <= 2) {
+    if (newNumber <= 2) {//Condiciones para cambiar de color del circulo 
         circleElement.style.backgroundColor = "#29e01f"; // Verde
-        uvBar.innerText = "Bajo";
+        uvBar.innerText = "Bajo"; //Usado para cambiar el texto segun el indice 
     } else if (newNumber <= 5) {
         circleElement.style.backgroundColor = "#d6e01f"; // Amarillo
         uvBar.innerText = "Moderado";
